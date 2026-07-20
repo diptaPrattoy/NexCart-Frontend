9f2b73865860245878160ae2bb3a4956fcffae75
𝙳𝚒𝚙𝚝𝚊 𝙿𝚛𝚊𝚝𝚝𝚘𝚢 𝙺𝚊𝚛𝚖𝚊𝚔𝚊𝚛 <145040028+Dipta395@users.noreply.github.com>
2026-07-20 22:16:10 +0600

    updated product page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,9 @@
-import ProductFilterBar from "@/components/ProductFilterBar";
 import React from "react";
 import ProductGrid from "@/components/ProductGrid";
 const page = () => {
   return (
     <div>
-      <ProductFilterBar
-        categories={[]}
-        selectedCategory={""}
-        priceFilter={"default"}
-      />
+
       <ProductGrid />
     </div>
   );
